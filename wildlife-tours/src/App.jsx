30f25a05603ce7cb378b84808a5e54a3f8f147b8
Brandyn Tse <brandyntse@protonmail.com>
2022-05-18 14:50:49 -0400

    created project nd setup with eslintrc, airbnb, prettier, and other misc extensions

---
+++ wildlife-tours/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import React from 'react';
+/* todo:
+- install and configure airbnb eslintrc
+- modify package json for code coverage and eslint
+- install react snippets
+- install live server
+- install react-router-dom for navbar links and stuff
+*/
+function App() {
+  return (
+    <div className="App" />
+  );
+}
+
+export default App;
