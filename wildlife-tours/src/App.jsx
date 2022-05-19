c2922193d2c2a88d0edcb223c2dcb4dd78d40eb1
Brandyn Tse <brandyntse@protonmail.com>
2022-05-19 13:50:14 -0400

    refactored package json for eslint airbnb and prettier

--- wildlife-tours/src/App.jsx
+++ wildlife-tours/src/App.jsx
@@ -1,15 +1,25 @@
-import './App.css';
-import React from 'react';
-/* todo:
-- install and configure airbnb eslintrc
-- modify package json for code coverage and eslint
-- install react snippets
-- install live server
-- install react-router-dom for navbar links and stuff
-*/
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
 function App() {
   return (
-    <div className="App" />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
