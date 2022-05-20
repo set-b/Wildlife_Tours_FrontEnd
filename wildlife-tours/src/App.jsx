41a72f1b789eb55f2cf8b65ddf88f57a4cd0e240
Brandyn Tse <brandyntse@protonmail.com>
2022-05-19 22:08:33 -0400

    added browser router and links

--- wildlife-tours/src/App.jsx
+++ wildlife-tours/src/App.jsx
@@ -1,24 +1,18 @@
 import React from "react";
-import logo from "./logo.svg";
+import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
+import HomePage from "./pages/HomePage/HomePage";
+import About from "./pages/AboutPage/AboutPage";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<HomePage />} />
+          <Route exact path="/about" element={<About />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ wildlife-tours/src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+// import { Style } from "./*.module.css";
+// see if new file will change the export default message
+function HomePage() {
+  return <div>HomePage</div>;
+}
+
+export default HomePage;
