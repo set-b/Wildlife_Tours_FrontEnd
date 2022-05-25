3a0d519a6677f2e8193dd61422e4471be57e6e7a
Brandyn Tse <brandyntse@protonmail.com>
2022-05-25 10:57:54 -0400

    removed unnecessary text, stylized p tags and wrapped text around img

--- wildlife-tours/src/pages/AboutPage/AboutPage.jsx
+++ wildlife-tours/src/pages/AboutPage/AboutPage.jsx
@@ -3,7 +3,7 @@ import pic from "../../assets/smiling.jpg";
 
 function AboutPage() {
   return (
-    <div>
+    <div style={{ position: "relative", top: "80px" }}>
       <img
         src={pic}
         alt=""
@@ -12,9 +12,10 @@ function AboutPage() {
           float: "left",
           width: "40%",
           height: "auto",
+          padding: "0 10px 0 0",
         }}
       />
-      {/* <p>
+      <p style={{ fontSize: "24px", textAlign: "justify", margin: "10px" }}>
         Wildlife Tours is a full-stack application based on a fictitious company
         created by Brandyn Tse. The front-end has been developed using React js
         framework and the material UI library. Assets have been compiled from
@@ -22,9 +23,25 @@ function AboutPage() {
         OpenShot, an open-source editor. For a demonstration of
         crud-functionality, as well as authentication/authorization, feel free
         to use the search and management functions found at the &quot;Book A
-        Tour&quot; section. If you are interested in contacting me, please refer
-        to the &quot;Contact&quot; section. Thank you!{" "}
-      </p> */}
+        Tour&quot; section. More of my github projects may also be found{" "}
+        <a
+          href="https://github.com/battafurai-gunmetaru"
+          style={{ color: "#33ccff", textDecoration: "none" }}
+        >
+          {" "}
+          here
+        </a>
+        . If you are interested in contacting me, please refer to the form under
+        the &quot;Contact&quot; section, or connect with me through{" "}
+        <a
+          href="https://www.linkedin.com/in/brandyn-tse-085872166"
+          style={{ color: "#33ccff", textDecoration: "none" }}
+        >
+          {" "}
+          linkedIn
+        </a>
+        . Thank you!{" "}
+      </p>
     </div>
   );
 }
--- wildlife-tours/src/pages/HomePage/HomePage.jsx
+++ wildlife-tours/src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import videoBg from "../../assets/wildlifefinal.mp4";
-// import { Style } from "./*.module.css";
-// see if new file will change the export default message
+
 function HomePage() {
   return (
     <div>
@@ -14,7 +13,6 @@ function HomePage() {
           objectFit: "contain",
           width: "80%",
           height: "auto",
-          // top: "15px",
         }}
       />
     </div>
