02bc183128e74e03abf0a09287d675e7339bd7ca
Brandyn Tse <brandyntse@protonmail.com>
2022-05-25 16:23:25 -0400

    made contact form

--- wildlife-tours/src/pages/Contact/Contact.jsx
+++ wildlife-tours/src/pages/Contact/Contact.jsx
@@ -1,7 +1,130 @@
-import React from "react";
+// import React from "react";
 
-function Contact() {
-  return <div>Contact form goes here with info about linkedin</div>;
+// function Contact() {
+//   return <div>Contact form goes here with info about linkedin</div>;
+// }
+
+// export default Contact;
+import * as React from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+// import FormControlLabel from "@mui/material/FormControlLabel";
+// import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import EmailIcon from "@mui/icons-material/Email";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+
+function Copyright(props) {
+  return (
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      // eslint-disable-next-line react/jsx-props-no-spreading
+      {...props}
+    >
+      {"Copyright © "}
+      <Link color="inherit" href="https://mui.com/">
+        Widlife Tours
+      </Link>{" "}
+      {new Date().getFullYear()}.
+    </Typography>
+  );
 }
+export default function Contact() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get("email"),
+      password: data.get("password"),
+    });
+  };
 
-export default Contact;
+  return (
+    <div style={{ position: "relative", top: "130px" }}>
+      <Container component="main" maxWidth="xs">
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: "white" }}>
+            <EmailIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Contact
+          </Typography>
+          <Box
+            component="form"
+            noValidate
+            onSubmit={handleSubmit}
+            sx={{ mt: 3 }}
+          >
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  autoComplete="given-name"
+                  name="firstName"
+                  required
+                  fullWidth
+                  id="firstName"
+                  label="First Name"
+                  autoFocus
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  required
+                  fullWidth
+                  id="lastName"
+                  label="Last Name"
+                  name="lastName"
+                  autoComplete="family-name"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  multiline
+                  rows={8}
+                  name="message"
+                  label="message"
+                  type="message"
+                  id="message"
+                />
+              </Grid>
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Send
+            </Button>
+          </Box>
+        </Box>
+        <Copyright sx={{ mt: 5 }} />
+      </Container>
+    </div>
+  );
+}
