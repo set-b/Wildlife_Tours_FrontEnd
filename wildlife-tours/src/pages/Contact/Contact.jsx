4a7d85a041ae27b14ecc3b758baf984404b98504
Brandyn Tse <brandyntse@protonmail.com>
2022-05-25 16:24:10 -0400

    refactored contact form code

--- wildlife-tours/src/pages/Contact/Contact.jsx
+++ wildlife-tours/src/pages/Contact/Contact.jsx
@@ -1,16 +1,7 @@
-// import React from "react";
-
-// function Contact() {
-//   return <div>Contact form goes here with info about linkedin</div>;
-// }
-
-// export default Contact;
 import * as React from "react";
 import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
-// import FormControlLabel from "@mui/material/FormControlLabel";
-// import Checkbox from "@mui/material/Checkbox";
 import Link from "@mui/material/Link";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
