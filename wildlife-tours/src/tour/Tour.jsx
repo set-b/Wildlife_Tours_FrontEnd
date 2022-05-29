9ff974c3f3338986a03b8c725f5979305cb52855
Brandyn Tse <brandyntse@protonmail.com>
2022-05-28 20:05:41 -0400

    added onhover playing through hover-video

--- wildlife-tours/src/tour/Tour.jsx
+++ wildlife-tours/src/tour/Tour.jsx
@@ -1,6 +1,5 @@
-import * as React from "react";
+import React from "react";
 import Grid from "@mui/material/Grid";
-// import Paper from "@mui/material/Paper";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
@@ -8,19 +7,12 @@ import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Tilt from "react-vanilla-tilt";
-// import Styling from "./Tour.module.css";
-
-// import FormControlLabel from "@mui/material/FormControlLabel";
-// import RadioGroup from "@mui/material/RadioGroup";
-// import Radio from "@mui/material/Radio";
+import HoverVideoPlayer from "react-hover-video-player";
+import Video from "../assets/wildlifefinal.mp4";
 
 export default function SpacingGrid() {
-  // populate cards as paper
-  // const options = {
-  //   scale: 1.3,
-  // };
   return (
-    <Grid sx={{ flexGrow: 1 }} container spacing={2}>
+    <Grid sx={{ flexGrow: 1 }} container spacing={7}>
       <Grid item xs={12} elevation={3}>
         <Grid container justifyContent="center" spacing={2}>
           {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((value) => (
@@ -38,15 +30,24 @@ export default function SpacingGrid() {
                   }}
                 >
                   <CardMedia
-                    component="img"
                     height="200"
-                    image="https://a-z-animals.com/media/2021/05/Favorite-and-Most-Popular-Animals_-Lion.jpg"
-                    alt="lion"
                     sx={{
                       transformStyle: "preserve-3d",
                       transform: "translateZ(60px)",
                     }}
-                  />
+                  >
+                    <HoverVideoPlayer
+                      videoSrc={Video}
+                      // playing={playing}
+                      muted
+                      loop
+                      style={{
+                        display: "block",
+                        maxWidth: "100%",
+                        height: "auto",
+                      }}
+                    />
+                  </CardMedia>
                   <CardContent>
                     <Typography gutterBottom variant="h5" component="div">
                       Lions in Africa
